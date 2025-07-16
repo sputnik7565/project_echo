8f4220e8bf130a2479f83bbd6aad2b569fc2ceae
Kim, Chan <59993977+sputnik7565@users.noreply.github.com>
2025-07-16 17:16:30 +0900

    1
    1

--- project_echo/settings.py
+++ project_echo/settings.py
@@ -20,10 +20,10 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# # Nginx의 proxy_set_header X-Forwarded-Proto $scheme; 설정을 활용
-# USE_X_FORWARDED_HOST = True
-# # 또는 스크립트 이름을 강제로 지정
-# FORCE_SCRIPT_NAME = '/echo'
+# Nginx의 proxy_set_header X-Forwarded-Proto $scheme; 설정을 활용 /로컬에선 주석처리 
+USE_X_FORWARDED_HOST = True
+# 또는 스크립트 이름을 강제로 지정
+FORCE_SCRIPT_NAME = '/echo'
 
 
 # Quick-start development settings - unsuitable for production
